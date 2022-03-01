2b7cbbeee488dfd45bff05f91b37ad8abca079b4
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-03-01 17:20:18 +0100

    API and methods for using tweets in CSV

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 require "json"
 require "open-uri"
+require "csv"
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-
+  TEXTS = []
   def call_twitter(query)
     #endpont fetch twits
     url = "https://api.twitter.com/2/tweets/search/recent?&query=#{query}&max_results=10&tweet.fields=lang"
@@ -13,21 +14,40 @@ class ApplicationController < ActionController::Base
     response = URI.open(url,
       "Authorization" => "Bearer AAAAAAAAAAAAAAAAAAAAAMjvZgEAAAAAzaTwcexM%2FozUoUW4TZjTDl30cpU%3D7oZ6mAazRU6drDRdUPTLllgfeXxKwl9OXwXjWHdyRuxgK23idR").read
     tweets = JSON.parse(response)["data"]
-    texts = []
-      tweets.each do |hash|
-          texts << hash["text"] if hash["lang"] == "en"
-        end
+    texts =[]
+    tweets.each do |hash|
+      texts << hash["text"] if hash["lang"] == "en"
+      end
       # array of strings = twits
-    return texts
+    return tweets
+  end
+
+  def split_texts(tweets)
+    content =[]
+    content = tweets.each do |tweet|
+      #array of strings
+      tweet.split(" ").flatten!
+      end
+    return content
+  end
+
+  def retrieve_score(words)
+    csv = "lib/assets/hedonometer.csv"
+
+    # response = File.open(csv)
+    scores =[]
+    words.each {|word| CSV.foreach(csv, headers: :first_row) do |row|
+      scores << row["Happiness Score"].to_i if row["Word"] == word
     end
-
-  def retrieve_score
-    # split each String in Array into words => (Array of words)
-    # ---- (filtering) ----
-    # Array of words.each => look for word in Hedononometer => save the score in an Array of scores (Integer)
-    # Make the average (of scores)
-
-    
+    }
+    sum = 0.0
+    scores.each { |num|
+      sum += num
+    }
+    sum = (sum / scores.count)
+    return sum.round(1)
   end
 
 end
+
+p retrieve_score([ "rape", "terrorist", "hitler"])
