49d467c40cfd13f63dbee0e25cc5902e1f02c26d
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-03-04 14:57:42 +0100

    improved show of a hashtag

--- app/javascript/components/hashtag_show.js
+++ app/javascript/components/hashtag_show.js
@@ -1,13 +1,22 @@
 var grabHashtag = () => {
-  var hashtags = document.querySelectorAll(".saved-hashtags")
+  var hashtags = document.querySelectorAll(".saved-hashtag-card")
   var container = document.querySelector(".output-info")
+  var name = document.querySelector("#hashtagname")
+  var date = document.querySelector("#hashtagdate")
+  var score = document.querySelector("#hashtagscore")
 
   hashtags.forEach((hash) => {
     hash.addEventListener('click', () => {
       // container.innerText = hash.hashtag
-      container.innerHTML = `<div class="bg bg-dark">${hash.innerHTML}</div>`
+      container.innerHTML = `
+      <div>
+        <div class="bg bg-light" id="name">${name.innerText}</div>
+        <div class="bg bg-primary" id="date">${date.innerText}</div>
+        <div class="bg bg-dark" id="score">${score.innerText}</div>
+      </div>
+      `
     })
-    console.log(hash.innerHTML)
+
   })
 
 }
