1cfe87ba13a4e910f2dc80782a7dd8a7b3f0bd0b
Eugenia Pastore <eugenia.pastore@gmail.com>
2022-03-09 22:56:23 +0100

    added styling to sad/happy words on show page from hashtag and cards

--- app/javascript/components/hashtag_show.js
+++ app/javascript/components/hashtag_show.js
@@ -1,12 +1,12 @@
 var grabHashtag = () => {
-  // stuff we need to select, 1 WE WANT TO MANIPULATE- tARGETING.
+  // stuff we need to select, WE WANT TO MANIPULATE- tARGETING.
   var hashtags = document.querySelectorAll(".saved-hashtag-card")
   var container = document.querySelector(".output-info")
   var id = document.querySelector("#hashtagid")
   var newBtns = document.querySelectorAll(".last-searched-hashtag-name")
+  var my_hash
 
-
-  // 2 ADD EVENT LISTENER -CLICK OR SCROLL, STAFF WE WANT TO EXECUTE
+  // ADD EVENT LISTENER -CLICK OR SCROLL, STAFF WE WANT TO EXECUTE
   newBtns.forEach((btn) => {
     btn.addEventListener('click', () => {
       var my_hash = ""
@@ -18,11 +18,7 @@ var grabHashtag = () => {
       })
       // container.innerText = hash.hashtag
       // initHashtagIndex()
-      // 3 EXECUTION OF WHAT I WANT TO DO
-
-
-
-
+      // 3EXECUTION OF WHAT I WANT TO DO
       container.innerHTML = `
       <div class="row show-hashtag-row">
         <div class="show-hashtag-name mt-3" id="name"><h2>${my_hash.querySelector("#hashtagname").innerText}</h2></div>
@@ -31,10 +27,26 @@ var grabHashtag = () => {
           <div class="p-0" id="score">${my_hash.querySelector("#hashtagscore").innerText}</div>
           <a class="button-secondary link-dashboard-secondary" href="/pdf.pdf?id=${id.innerText}">Download PDF</a>
         </div>
-        <div class="col-6 mt-3 show-card-right text-center">
-          <p class="mb-0">At this moment the score is</p>
-          <div class="p-0" id="score">?</div>
+        <div class="col-6 mt-3">
+          <h4 class="search-hashtag-list-header">Happiest words &#128515</h4>
+            <div class="search-hashtag-list green-shadow">
+              <div class="p-0" id="hashtaghappywords">
+              <p class="search-hashtag-happysad">
+              ${JSON.parse(my_hash.querySelector("#hashtaghappywords").innerText).map(item => {
+                return `<p class="search-hashtag-happysad">${item}</p>`;
+              }).join('')}
+              </div>
+            </div>
 
+            <h4 class="search-hashtag-list-header mt-3">Saddest words &#128148</h4>
+            <div class="search-hashtag-list green-shadow">
+              <div class="p-0" id="hashtaghappywords">
+              <p class="search-hashtag-happysad">
+              ${JSON.parse(my_hash.querySelector("#hashtagsadwords").innerText).map(item => {
+                return `<p class="search-hashtag-happysad">${item}</p>`;
+              }).join('')}
+              </div>
+            </div>
       </div>
       `
     })
@@ -54,6 +66,7 @@ var grabHashtag = () => {
         happy_arr = JSON.parse(hash.querySelector("#hashtaghappywords").innerText)
       }
 
+      console.log(sad_arr)
       container.innerHTML = `
       <div class="row show-hashtag-row">
         <div class="show-hashtag-name mt-3" id="name"><h2>${hash.querySelector("#hashtagname").innerText}</h2></div>
@@ -62,22 +75,29 @@ var grabHashtag = () => {
           <div class="p-0" id="score">${hash.querySelector("#hashtagscore").innerText}</div>
           <a class="button-secondary link-dashboard-secondary" href="/printshow.pdf?id=${id.innerText}">Download PDF</a>
         </div>
-        <div class="col-6 mt-3 show-card-right text-center">
-        <div class="p-0" id="hashtaghappywords">
-          <ul>
+        <div class="col-6 mt-3">
+          <h4 class="search-hashtag-list-header">Happiest words &#128515</h4>
+          <div class="search-hashtag-list green-shadow">
+            <div class="p-0" id="hashtaghappywords">
+            <p class="search-hashtag-happysad">
             ${happy_arr.map(item => {
-              return `<li>${item}</li>`;
+              return `<p class="search-hashtag-happysad">${item}</p>`;
             }).join('')}
-          </ul>
-        </div>
-          <div class="p-0" id="hashtagsadwords">
-            <ul>
-              ${sad_arr.map(item => {
-                return `<li>${item}</li>`;
-              }).join('')}
-            </ul>
+
+            </div>
           </div>
-        </div>
+
+          <h4 class="search-hashtag-list-header mt-3">Saddest words &#128148</h4>
+          <div class="search-hashtag-list green-shadow">
+            <div class="p-0" id="hashtaghappywords">
+            <p class="search-hashtag-happysad">
+            ${sad_arr.map(item => {
+              return `<p class="search-hashtag-happysad">${item}</p>`;
+            }).join('')}
+            </div>
+          </div>
+
+
       </div>
       `
     })
